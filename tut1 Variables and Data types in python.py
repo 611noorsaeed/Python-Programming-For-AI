ed11f613f035dbeab7887877d5c6913a560f4019
Noor Saeed <124183389+611noorsaeed@users.noreply.github.com>
2024-07-17 01:06:36 -0700

    Add files via upload

---
+++ tut1 Variables and Data types in python.py
@@ -0,0 +1,37 @@
+# program 1: simple calculator
+
+# num1 = 10
+# num2 = 5
+#
+# add = num1 + num2
+# sub = num1 - num2
+# mulp = num1 * num2
+# div = num1 / num2
+# mod = num1 % num2
+# print("Addition ", add)
+# print("substraction ", sub)
+# print("Multiplication ", mulp)
+# print("div", div)
+# print("mode ", mod)
+
+
+# Program 2: Greeting Message
+
+# name = "Noor Saeed"
+# greeting = "How are you"
+# print(name)
+# print(greeting)
+
+
+# Program 3: List Operations
+# fruits = ["apple", "banana", "cherry"]
+#
+# # adding an element
+# fruits.append('orange')
+# print(fruits)
+# fruits.remove('banana')
+# print(fruits)
+#
+# new_var = fruits[0]
+# print(new_var)
+
